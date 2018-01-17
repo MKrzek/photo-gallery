b4f28c6bafbe5849926871705704ca29dda3873b
Malgorzata <mkrzek@googlemail.com>
2018-01-17 19:14:32 +0000

    added initial settings

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./store.js";
+import registerServiceWorker from "./registerServiceWorker";
+
+import App from "./components/App.js";
+
+
+const store = configureStore();
+console.log("store", store);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+registerServiceWorker();
